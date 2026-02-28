1d17ac5c18d88e9a4948c26c3566585898d880ee
Blackbean28 <blackbean2343@gmail.com>
2026-02-28 20:03:17 +0900

    버튼 추가, 상태창 추가

--- SBS_ECS_UI/SBS_ECS_UI/Models/TargetInfo.cs
+++ SBS_ECS_UI/SBS_ECS_UI/Models/TargetInfo.cs
@@ -1,10 +1,11 @@
-﻿using SBS_ECS_UI.ViewModels;
+﻿using System;
+using SBS_ECS_UI.ViewModels;
 
 namespace SBS_ECS_UI.Models
 {
     public class TargetInfo : ViewModelBase
     {
-        private string _name; // 💡 이름 속성 추가
+        private string _name;
         private float _posX_mm;
         private float _posY_mm;
         private float _azimuthDegree;
@@ -18,13 +19,23 @@ namespace SBS_ECS_UI.Models
         public float PosX_mm
         {
             get => _posX_mm;
-            set { _posX_mm = value; onPropertyChanged(); }
+            set
+            {
+                _posX_mm = value;
+                onPropertyChanged();
+                onPropertyChanged(nameof(Distance)); // 💡 X가 바뀌면 거리도 새로 고침
+            }
         }
 
         public float PosY_mm
         {
             get => _posY_mm;
-            set { _posY_mm = value; onPropertyChanged(); }
+            set
+            {
+                _posY_mm = value;
+                onPropertyChanged();
+                onPropertyChanged(nameof(Distance)); // 💡 Y가 바뀌면 거리도 새로 고침
+            }
         }
 
         public float AzimuthDegree
@@ -32,5 +43,9 @@ namespace SBS_ECS_UI.Models
             get => _azimuthDegree;
             set { _azimuthDegree = value; onPropertyChanged(); }
         }
+
+        // 💡 실시간 거리 계산 속성 (피타고라스 정리)
+        // $$Distance = \sqrt{PosX^2 + PosY^2}$$
+        public float Distance => (float)Math.Sqrt(Math.Pow(PosX_mm, 2) + Math.Pow(PosY_mm, 2));
     }
 }
